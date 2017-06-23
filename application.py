4d3baba339af620e10d5ba2cf03a86e5e9d6b884
sandervandorsten <sandervandorsten@gmail.com>
2017-06-23 22:51:21 +0200

    added jquery en templates

--- application.py
+++ application.py
@@ -1,34 +1,51 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
 
 # print a nice greeting.
 def say_hello(username = "World"):
     return '<p>Hello %s!</p>\n' % username
 
-# some bits of text for the page.
-header_text = '''
-    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
-instructions = '''
-    <p><em>Hint</em>: This is a RESTful web service! Append a username
-    to the URL (for example: <code>/Thelonious</code>) to say hello to
-    someone specific.</p>\n'''
-home_link = '<p><a href="/">Back</a></p>\n'
-footer_text = '</body>\n</html>'
+def hoi():
+	# some bits of text for the page.
+	header_text = '''
+        <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+	instructions = '''
+        <p><em>Hint</em>: This is a RESTful web service! Append a username
+        to the URL (for example: <code>/Thelonious</code>) to say hello to
+        someone specific.</p>\n'''
+	home_link = '<p><a href="/">Back</a></p>\n'
+	footer_text = '</body>\n</html>'
 
-# EB looks for an 'application' callable by default.
-application = Flask(__name__)
+	# EB looks for an 'application' callable by default.
 
-# add a rule for the index page.
-application.add_url_rule('/', 'index', (lambda: header_text +
-    say_hello() + instructions + footer_text))
+	# add a rule for the index page.
+	application.add_url_rule('/', 'index', (lambda: header_text +
+													say_hello() + instructions + footer_text))
 
-# add a rule when the page is accessed with a name appended to the site
-# URL.
-application.add_url_rule('/<username>', 'hello', (lambda username:
-    header_text + say_hello(username) + home_link + footer_text))
+	# add a rule when the page is accessed with a name appended to the site
+	# URL.
+	application.add_url_rule('/<username>', 'hello', (lambda username:
+													  header_text + say_hello(username) + home_link + footer_text))
+
+
+@app.route("/")
+def hello():
+	style    = url_for('static', filename ='style.css')
+	jquery   = url_for('static', filename='jquery-1.11.2.min.js')
+	return render_template('index.html', style = style, jquery = jquery)
+
+# @app.route("/run", methods=['POST'])
+# def run():
+# 	if request.method == 'POST':
+# 		#print request.form['code'].split("\n")
+# 		robotParser.parseText(request.form['code'].split("\n"))
+# 		return "Oke!"
+
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
-    application.debug = True
-    application.run()
+    app.debug = True
+    app.run()
